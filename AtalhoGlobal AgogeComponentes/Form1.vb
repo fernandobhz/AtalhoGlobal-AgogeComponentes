b4ef2b2158e66b11f1258a79ee46bb977430753a
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-07-25 18:56:19 -0300

    old project migrated

---
+++ AtalhoGlobal AgogeComponentes/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Inherits AgogeComponents.Formulario
+
+    Sub New()
+        Me.InitializeComponent()
+        Me.RegistrarAtalhoGlobal(AgogeComponents.HKey.Mods.Shift Or AgogeComponents.HKey.Mods.Ctrl, Keys.I, AddressOf Me.Iniciar)
+    End Sub
+
+
+    Sub Iniciar()
+        Me.FocusMe()
+        MsgBox("OI")
+        'Dim f As New Form
+        'f.ShowDialog()
+    End Sub
+
+End Class
